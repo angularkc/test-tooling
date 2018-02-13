8f45647c7b4b7f2918e0f73e20240e16081f9e68
Steven Barnhurst <stevenbarnhurst@gmail.com>
2018-02-13 17:20:09 -0600

    here

---
+++ src/helpers/store.mock.ts
@@ -0,0 +1,16 @@
+import { Action, Store } from '@ngrx/store';
+import { Subject } from 'rxjs/Subject';
+
+export class MockStore<T> {
+  actions?: Subject<Action>;
+  states?: Subject<T>;
+}
+
+export function mockStore<T>({actions = new Subject<Action>(), states = new Subject<T>()}: MockStore<T>): Store<T> {
+  const result = states as any;
+  result.select = cb => result.map(cb);
+  result.dispatch = (action: Action) => actions.next(action);
+  return result;
+}
+
+export const getMockStore = () => mockStore<any>({});
