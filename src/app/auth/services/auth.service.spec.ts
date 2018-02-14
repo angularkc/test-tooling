b078e24c062bd678315ecf413f3c2af69be2a6b3
Steven Barnhurst <stevenbarnhurst@gmail.com>
2018-02-13 18:33:25 -0600

    here

--- src/app/auth/services/auth.service.spec.ts
+++ src/app/auth/services/auth.service.spec.ts
@@ -1,5 +1,10 @@
 import {TestBed, inject} from '@angular/core/testing';
 import {AuthService} from './auth.service';
+import {Store} from '@ngrx/store';
+import {getMockStore} from '../../../helpers/store.mock';
+import {expressionChangedAfterItHasBeenCheckedError} from "@angular/core/src/view/errors";
+
+let store: Store<any>;
 
 describe('AuthService', () => {
 
@@ -8,20 +13,33 @@ describe('AuthService', () => {
       imports: [
       ],
       providers: [
-        AuthService
+        AuthService,
+        {
+          provide: Store,
+          useValue: getMockStore()
+        }
       ]
     });
+    store = TestBed.get(Store);
   });
 
   describe('login', () => {
     it('should dispatch Login', inject([AuthService], (service: AuthService) => {
-      expect(true).toBe(false);
+      const storeSpy = spyOn(store, 'dispatch');
+
+      service.login();
+
+      expect(storeSpy).toHaveBeenCalled();
     }));
   });
 
   describe('isAuthenticated', () => {
-    it('should return false when token is expired', inject([AuthService], (service: AuthService) => {
-      expect(true).toBe(false);
+    it('should return false when not authed', inject([AuthService], (service: AuthService) => {
+      service.isAuthed = false;
+
+      const result = service.isAuthenticated();
+
+      expect(result).toBeFalsy();
     }));
   });
 
